bcc6d20b3fdfd6c7367b9b7d829832ea268abbbc
andrew72-max <andrew.nyongesa@student.moringaschool.com>
2025-01-17 17:48:31 +0300

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import user from "../data/user";
 function App() {
   return (
     <div>
