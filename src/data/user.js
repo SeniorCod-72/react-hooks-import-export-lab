8d91d2ddd660c4457f3ea90974ebdeae171b62af
SeniorCod-72 <wwekesa72@gmail.com>
2025-02-04 14:40:58 +0300

    done

--- src/data/user.js
+++ src/data/user.js
@@ -1,8 +1,8 @@
-
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
-export default {username,city,image};
+
+export {username ,city, image };
